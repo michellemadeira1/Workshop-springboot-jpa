2c73b202d7315320d4dc393c9c597a98f467d547
Michelle Madeira <michellemadeira1@gmail.com>
2022-11-30 11:58:29 -0300

    Primeiro commit

---
+++ src/main/java/com/portifolio/projetobackend2/ProjetoBackend2Application.java
@@ -0,0 +1,13 @@
+package com.portifolio.projetobackend2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBackend2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBackend2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/portifolio/projetobackend2/ProjetoBackend2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portifolio.projetobackend2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBackend2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
